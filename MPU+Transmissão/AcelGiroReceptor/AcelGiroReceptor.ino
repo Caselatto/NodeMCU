cb2a780d9c4201b1f70800e2e0ec762dce76a03e
Matheus Caselatto <matheuscaselatto@gmail.com>
2020-05-21 18:06:31 -0300

    Receptor OK
    WebServer pega os valores convertidos enviados para a função e armazena

--- MPU+Transmissão/AcelGiroReceptor/AcelGiroReceptor.ino
+++ MPU+Transmissão/AcelGiroReceptor/AcelGiroReceptor.ino
@@ -1,1 +1,28 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
+// Armazenamento dos dados após conversão
+double AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+// Parametros de configuração para Access Point
+const char *ssid = "RedeWIFI_NodeMCU";
+const char *password = "123456789";
+IPAddress ip(192, 168, 11, 4);
+IPAddress gateway(192, 168, 11, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+ESP8266WebServer server; // Define o objeto server
+
+void setup()
+{
+  Serial.begin(9600);
+  setup_transmissao();
+}
+
+void loop()
+{
+  server.handleClient();
+  mostraCON();
+
+  Serial.println();
+}
--- MPU+Transmissão/AcelGiroReceptor/Transmissao.ino
+++ MPU+Transmissão/AcelGiroReceptor/Transmissao.ino
@@ -1,1 +1,33 @@
+void setup_transmissao()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid, password);
 
+  // Print IP for check
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // Configure the server's route
+  server.on("/", handleIndex); //Receber requisições, funciona como interrupções
+  server.on("/atual", atualizar);
+  server.begin();
+}
+
+void handleIndex()
+{
+    server.send(200, "text/plain", String(millis())); //Needs refresh for update
+  //  Serial.print(millis());
+}
+
+void atualizar()
+{
+  AcX = server.arg("AcX").toFloat();
+  AcY = server.arg("AcY").toFloat();
+  AcZ = server.arg("AcZ").toFloat();
+
+  GyX = server.arg("GyX").toFloat();
+  GyY = server.arg("GyY").toFloat();
+  GyZ = server.arg("GyZ").toFloat();
+}
