9b127cbe38ff47d0c5790e81260231ea8a2cc12d
Matheus Caselatto <matheuscaselatto@gmail.com>
2020-06-10 18:15:57 -0300

    Commit inicial
    Arquivo funcional de criação do arquivo e gravação no cartão SD

---
+++ Cartao/Cartao.ino
@@ -0,0 +1,82 @@
+//biblioteca responsável pela comunicação com o Cartão SD
+#include "SD.h"
+
+//PINOUT
+/* VCC        VCC
+   GND        GND
+   MISO       D6
+   MOSI       D7
+   SCK        D5
+*/
+#define CS_PIN  D8  //pino ligado ao CS do módulo SD Card
+
+
+char filename[7] = "00.TXT";    // Nome do arquivo inicial
+int i = 0;
+
+float valor;
+File sdFile;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(CS_PIN, OUTPUT);   // Inicia cartão SD
+  if (!SD.begin(CS_PIN))
+  {
+    Serial.println(F("SD falhou"));   // Em caso de falha na inicialização
+    while (1);
+  }
+  Serial.println(F("Tudo certo"));    // Inicialização certa
+
+  while (SD.exists(filename))         // Função para gerar arquivo com nome seguinte
+  {
+    if (i < 10)
+    {
+      sprintf(filename, "0%d.TXT", i);
+    }
+    else
+    {
+      sprintf(filename, "%2d.TXT", i);
+    }
+    i++;
+  }
+  sdFile = SD.open(filename, FILE_WRITE);
+  Serial.print("Arquivo: ");
+  Serial.println(filename);
+  Serial.println("SD ok");
+
+  valor = millis();
+}
+
+void mostra (float valor)
+{
+  Serial.print(valor);
+  Serial.print("\t");
+  sdFile.print(valor);
+  sdFile.print(",");
+}
+
+void loop ()
+{
+  valor = millis() - valor;
+  //  sdFile = SD.open(filename, FILE_WRITE);
+  if (sdFile)
+  {
+    Serial.println("O arquivo foi aberto com sucesso.");
+    mostra(valor / 1000);
+    //    sdFile.println(valor);
+    //    sdFile.print(" | ");
+    //    sdFile.print(valor / 1000);
+    sdFile.println();
+    //    sdFile.close();
+  }
+  else
+  {
+    Serial.println("Falha ao abrir o cartão");
+  }
+
+  sdFile.flush();
+
+  //  delay(500);
+}
